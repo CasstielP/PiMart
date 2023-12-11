7a6c780c6b52740b0d15f2cfb1a014633f6dd724
Casstiel <cpss5433@gmail.com>
2023-12-10 21:46:54 -0800

    navbar dropdown

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from './piMart_logo.png'
+import LoginSection from './loginSection';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
+	const [showDropDown, setShowDropDown] = useState(false)
 
+	const handleMouseEnter = () => {
+        setShowDropDown(true)
+    }
+
+    const handleMouseLeave = () => {
+        setShowDropDown(false)
+    }
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">
-					PiMart
+		<>
+			<div className='navbar'>
+				<div id='siteLogoContainer'>
+					<NavLink exact to="/">
+						<img id='siteLogo' src={logo} alt='page logo'></img>
 					</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+				</div>
+				{isLoaded && (
+					<>
+						<div className='navbar-right'>
+							<div className='nabar-right-items'>
+								<div id='signInContainer'
+									onMouseEnter={handleMouseEnter}
+									onMouseLeave={handleMouseLeave}
+								>
+									<div>
+										hello, sign in
+									</div>
+									<div id='AcListContainer'>
+										Account & Lists
+										<span class="material-symbols-outlined">
+											arrow_drop_down
+										</span>
+									</div>
+									{showDropDown && (
+										<LoginSection
+										// user={sessionUser}
+										// showDropDown={showDropDown}
+										/>
+									 )}
+								</div>
+								{/* <ProfileButton user={sessionUser} /> */}
+							</div>
+							<div className='nabar-right-items'>
+								return's & orders
+							</div>
+							<div className='nabar-right-items'>
+								cart
+							</div>
+						</div>
+					</>
+				)}
+			</div>
+		</>
 	);
 }
 
---
+++ react-app/src/components/Navigation/loginSection/index.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import './index.css'
+
+function LoginSection ({showMenu}) {
+    const dispatch = useDispatch();
+
+    return (
+        <>
+            <div className="login-dropdown">
+                    {/* showing */}
+            </div>
+        </>
+    )
+
+
+
+}
+
+
+export default LoginSection
